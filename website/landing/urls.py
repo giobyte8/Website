c8515377cee1adbde623e30882e6127d92b3c109
Giovanni Aguirre <giovanni.fi05@gmail.com>
2019-05-18 19:42:31 -0500

    :rocket: Basic structure and navigation flow

--- website/landing/urls.py
+++ website/landing/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('portfolio', views.portfolio, name='portfolio'),
+    path('learning', views.learning, name='learning'),
+    path('blog', views.blog, name='blog')
 ]
--- website/landing/views.py
+++ website/landing/views.py
@@ -4,4 +4,24 @@ from django.shortcuts import render
 
 
 def index(req):
-    return render(req, 'index/index.html')
+    return render(req, 'index/index.html', {
+        'nav_active': 'index'
+    })
+
+
+def portfolio(req):
+    return render(req, 'portfolio/side_projects.html', {
+        'nav_active': 'portfolio'
+    })
+
+
+def learning(req):
+    return render(req, 'portfolio/continuous_learning.html', {
+        'nav_active': 'learning'
+    })
+
+
+def blog(req):
+    return render(req, 'blog/index.html', {
+        'nav_active': 'blog'
+    })
