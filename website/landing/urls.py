5106ffaa93f541318523772f32bb59f5c213d1be
Giovanni Aguirre <gaguirre@skytouchtechnology.com>
2021-05-12 21:28:39 -0500

    Cleaning up

--- website/landing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('portfolio', views.portfolio, name='portfolio'),
-    path('learning', views.learning, name='learning'),
-    path('blog', views.blog, name='blog')
-]
