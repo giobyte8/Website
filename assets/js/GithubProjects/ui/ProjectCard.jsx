21dd92d6d2320d718f0cf1ae9f10f7405e5f7309
Giovanni Aguirre <gaguirre@skytouchtechnology.com>
2021-06-08 22:25:17 -0500

    Use production react when building site for prod env

--- assets/js/GithubProjects/ui/ProjectCard.jsx
+++ assets/js/GithubProjects/ui/ProjectCard.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import * as React from 'react'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCodeBranch, faStar } from '@fortawesome/free-solid-svg-icons'
 
